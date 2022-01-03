e4b44e85528a85db4b1153ac165c3b5ea5a2a19a
Anamika_s <anamika.sawhney22@gmail.com>
2022-01-03 11:38:33 +0530

    IteratorDemo

---
+++ Aggregate.cs
@@ -0,0 +1,6 @@
+using System;
+
+  public abstract class Aggregate
+    {
+        public abstract Iterator CreateIterator();
+    }
---
+++ ConcreteAggregate.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+    public class ConcreteAggregate : Aggregate
+    {
+        List<object> items = new List<object>();
+        public override Iterator CreateIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+        // Get item count
+        public int Count
+        {
+            get { return items.Count; }
+        }
+        // Indexer
+        public object this[int index]
+        {
+            get { return items[index]; }
+            set { items.Insert(index, value); }
+        }
+    }
